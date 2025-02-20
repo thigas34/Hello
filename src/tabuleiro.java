bfa7dbbea6e448d564b88e1f957a5dbdc26577d2
Copperlamb78 <nascimentogabriel.2004@gmail.com>
2025-02-20 20:17:47 -0300

    adicionando cores e valores

--- src/tabuleiro.java
+++ src/tabuleiro.java
@@ -1,21 +1,36 @@
+import java.util.Random;
+
 public class tabuleiro {
     public static void mostrartabuleiro() {
-        // Código ANSI para as cores
-        String azul = "\033[44m";  // Fundo azul
-        String vermelho = "\033[41m";  // Fundo vermelho
-        String reset = "\033[0m";  // Reseta a cor para o padrão
 
-        for(int linha = 0; linha < Configuraçoes.tamanhoTabuleiro; linha++) {
-            for(int coluna = 0; coluna < Configuraçoes.tamanhoTabuleiro; coluna++) {
-                if ((linha + coluna) % 2 == 0) {
-                    System.out.print(azul + "   " + reset);
-                } else {
-                    System.out.print(vermelho + "   " + reset);
+        String azul = "\u001b[44m";
+        String vermelho = "\u001b[41m";
+        String preto = "\u001b[40m";
+        String amarelo = "\u001b[43m";
+        String branco = "\u001b[47m";
+        String reset = "\u001b[0m";
+
+        String[] cores = { azul, vermelho, preto, amarelo };
+
+        Random random = new Random();
+
+        for (int linha = 0; linha < (Configuraçoes.tamanhoTabuleiro + 1); linha++) { // Contagem de linhas
+
+            if (linha != 0) {
+                System.out.print(linha + " ");
+            }
+
+            for (int coluna = 0; coluna < Configuraçoes.tamanhoTabuleiro; coluna++) { // Contagem de colunas
+
+                if (linha == 0) {
+                    System.out.print("   " + (coluna + 1));
+                    continue;
                 }
+
+                String corAleatoria = cores[random.nextInt(cores.length)];
+                System.out.print(corAleatoria + "    " + reset);
             }
             System.out.println();
-            
         }
-
     }
 }
